153bef61b59e2c6cfb022ad859a41e81d72f4746
KaylaMoira <afreenmohideen@gmail.com>
2021-12-02 11:39:50 -0500

    change statusbar up

--- client/src/components/Statusbar.js
+++ client/src/components/Statusbar.js
@@ -4,6 +4,7 @@ import { Typography } from '@mui/material'
 import { Fab } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add';
 import IconButton from '@mui/material/IconButton';
+import AuthContext from '../auth'
 
 /*
     Our Status bar React component goes at the bottom of our UI.
@@ -12,23 +13,35 @@ import IconButton from '@mui/material/IconButton';
 */
 function Statusbar() {
     const { store } = useContext(GlobalStoreContext);
+    const { auth } = useContext(AuthContext);
+    
     let text ="";
-    if (store.currentList)
-        text = store.currentList.name;
+    let addButton = "";
+    // if (store.currentList)
+    //     text = store.currentList.name;
     
     const handleCreateNewList = () => {
         console.log("making list...")
         store.createNewList();
     }
+
+    if(auth.user){
+        text = "Your Lists";
+        addButton = (
+            <IconButton onClick={handleCreateNewList} >
+                <AddIcon sx={{ fontSize: '75px', color: "black"}}/>
+            </IconButton>
+            )
+    }
+    
+
     return (
         // <div id="top5-statusbar">
         //     <Typography variant="h4">{text}</Typography>
         // </div>
         <div id="top5-statusbar">
-            <IconButton onClick={handleCreateNewList} >
-            <AddIcon sx={{ fontSize: '75px', color: "black"}}/>
-            </IconButton>
-            <Typography variant="h2">Your Lists</Typography>
+            {addButton}
+            <Typography variant="h2">{text}</Typography>
         </div>
     );
 }
