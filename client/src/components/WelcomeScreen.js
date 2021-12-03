6437daa0f4dc0bafc9a49b3a8942f05843db1ef7
KaylaMoira <afreenmohideen@gmail.com>
2021-12-03 18:48:06 -0500

    styling welcome screen

--- client/src/components/WelcomeScreen.js
+++ client/src/components/WelcomeScreen.js
@@ -3,6 +3,8 @@ import Button from '@mui/material/Button';
 import AuthContext from '../auth'
 import { GlobalStoreContext } from '../store'
 import { Link } from 'react-router-dom'
+import Grid from "@mui/material/Grid";
+import Typography from '@mui/material/Typography';
 
 export default function WelcomeScreen() {
     const { auth } = useContext(AuthContext);
@@ -24,7 +26,32 @@ export default function WelcomeScreen() {
 
     return (
         <div id="welcome-screen">
-            <h>WELCOME<br />
+            <Grid container spacing={2}>
+                <Grid item xs={10}>
+                    <Typography sx={{fontSize:"72pt"}}>WELCOME TO THE TOP 5 LISTER</Typography>
+                </Grid>
+                <Grid item xs={2}>
+                    <Typography></Typography>
+                </Grid>
+                <Grid item xs={10}>
+                    <Typography sx={{fontSize:"48pt"}}>This site lets you create top five lists and interact
+                with the lists of other users!</Typography>
+                </Grid>
+                <Grid item xs={2}>
+                    <Typography></Typography>
+                </Grid>
+                <Grid item xs={4} sx={{marginLeft:"150px"}}>
+                    <Button class="welcome-button"><Link to='/register/' style={{ textDecoration: 'none', color: "black" }}>Create New Account</Link></Button><br />
+                </Grid>
+                <Grid item xs={2}>
+                    <Button class="welcome-button"><Link to='/login/' style={{ textDecoration: 'none', color: "black" }}>Login</Link></Button><br />
+                </Grid>
+                <Grid item xs={4}>
+                    <Button class="welcome-button" onClick={handleGuestLogin}>Continue as Guest</Button><br />
+                </Grid>
+
+            </Grid>
+            {/* <h>WELCOME<br />
             TO THE</h> <br/>
             <h>TOP 5 LISTER</h><br/>
             <h>This site lets you create top five lists and interact
@@ -32,7 +59,7 @@ export default function WelcomeScreen() {
             </h>
             <Button class="welcome-button"><Link to='/register/' style={{ textDecoration: 'none', color: "black" }}>Create New Account</Link></Button><br />
             <Button class="welcome-button"><Link to='/login/' style={{ textDecoration: 'none', color: "black" }}>Login</Link></Button><br />
-            <Button class="welcome-button" onClick={handleGuestLogin}>Continue as Guest</Button><br />
+            <Button class="welcome-button" onClick={handleGuestLogin}>Continue as Guest</Button><br /> */}
         </div>
     )
 }
